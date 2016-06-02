59e26d56ea16aa6e1b814fd24692e9683cdc1a86
zmj <zmj@zmjdemacbook-pro.local>
2016-06-02 21:44:47 +0800

    Initial Commit

---
+++ BZLib/BZLib.h
@@ -0,0 +1,19 @@
+//
+//  BZLib.h
+//  BZLib
+//
+//  Created by zmj on 16/6/2.
+//  Copyright © 2016年 zmj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BZLib.
+FOUNDATION_EXPORT double BZLibVersionNumber;
+
+//! Project version string for BZLib.
+FOUNDATION_EXPORT const unsigned char BZLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BZLib/PublicHeader.h>
+
+
